c702e31b634540c7fa366389a4fadbad1c221f41
EdaKevser Demir <eda_kevser_@hotmail.com>
2022-05-15 01:21:44 +0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>selam bebekooo :*</Text>
+      <Text>selam bebekooo </Text>
       <StatusBar style="auto" />
     </View>
   );
