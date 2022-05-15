c9c97936b4f95a8441b92f925a8646650b6dbbb1
EdaKevser Demir <eda_kevser_@hotmail.com>
2022-05-15 18:39:26 +0300

    banner added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {StyleSheet, Text, View,FlatList,SafeAreaView} from 'react-native';
+import {Image,ScrollView,StyleSheet, Text, View,FlatList,SafeAreaView} from 'react-native';
 import NewsCard from './components/NewsCard';
 
 import news_data from './news_data.json';
+import news_banner_data from './news_banner_data.json'
+import { Dimensions } from 'react-native';
 
 function App() {
 
@@ -10,8 +12,21 @@ function App() {
   const renderNews=({item})=><NewsCard news={item}/>
  
   return (   
+    
     <View style={styles.container}>
+     
+      <Text style={styles.headerText}>NEWS</Text>
      <FlatList
+     ListHeaderComponent={()=>
+     <ScrollView
+      horizontal showsHorizontalScrollIndicator={false}>
+     {
+       news_banner_data.map(bannerNews=> 
+       <Image style={styles.bannerImage} source={{uri:bannerNews.imageUrl}} />)
+     }
+
+     </ScrollView>}
+
      keyExtractor={item=>item.u_id.toString()}
        data={news_data}
        renderItem={renderNews}
@@ -24,9 +39,22 @@ function App() {
   const styles=StyleSheet.create({
    
     container:{
-     flex:1,
+    flex:1,
      backgroundColor:'pink'
- }
+ },
+ bannerImage:{
+   height:Dimensions.get('window').height/4,
+   width:Dimensions.get('window').width/2
+ },
+ headerText:{
+   fontSize:50,
+   fontWeight:'bold',
+   fontStyle:'italic',
+   marginTop:25,
+   textAlign:'center'
+   
+ },
+
  
  });
   export default App;
